b147edfa94facb54165fb5bf47552313c8de4e12
AnsyGitHub <ansy.prof@gmail.com>
2024-07-24 15:23:53 +0500

    Refactor test cases

--- JUnit/src/test/java/helper/StringHelperTest.java
+++ JUnit/src/test/java/helper/StringHelperTest.java
@@ -6,10 +6,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringHelperTest {
 
+    StringHelper helper = new StringHelper();
+
     @Test
-    public void test() {
-        StringHelper helper = new StringHelper();
+    public void testTruncateAInFirst2Positions_AInFirst2Positions() {
         assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+    }
+
+    @Test
+    public void testTruncateAInFirst2Positions2_AInFirstPosition() {
         assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+
     }
 }
