608217fbdbc3f10ca782af9ace29641c2ea49b73
AnsyGitHub <ansy.prof@gmail.com>
2024-07-24 14:36:41 +0500

    Write first unit test using JUnit

---
+++ JUnit/src/test/java/helper/StringHelperTest.java
@@ -0,0 +1,15 @@
+package helper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringHelperTest {
+
+    @Test
+    public void test() {
+        StringHelper helper = new StringHelper();
+        assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+        assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+    }
+}
